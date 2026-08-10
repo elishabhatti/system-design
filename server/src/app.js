2eccba8798bc7d01282668919dcdab1d94f98dc0
elisha <elishajameel2007@gmail.com>
2026-08-10 10:29:04 +0500

    added express static middleware to serve videos

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import videoRoutes from './routes/video.routes.js';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
+app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 app.use('/api/videos', videoRoutes);
 
 export { app };
