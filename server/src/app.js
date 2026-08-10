4121ff44dee755b558f056c9e51920a8225d072d
elisha <elishajameel2007@gmail.com>
2026-08-10 10:17:41 +0500

    enabled cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import videoRoutes from './routes/video.routes.js';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
 app.use('/api/videos', videoRoutes);
