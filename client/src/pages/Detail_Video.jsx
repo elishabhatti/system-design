3b94ddd92f0c51bd4ebc2ab102d85fbb32448747
elisha <elishajameel2007@gmail.com>
2026-08-11 20:52:36 +0500

    added few pages that are needed to the next phase

---
+++ client/src/pages/Detail_Video.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DetailedVideo = () => {
+  return (
+    <div>DetailedVideo</div>
+  )
+}
+
+export default DetailedVideo
---
+++ client/src/pages/User_Videos_Page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserVideosPage = () => {
+  return (
+    <div>UserVideosPage</div>
+  )
+}
+
+export default UserVideosPage
