aa8797d66d5bba2ed86de7ae2a25a5b5ff4cef68
elisha <elishajameel2007@gmail.com>
2026-08-13 10:11:38 +0500

    fixed detailed video name

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import CreateVideo from './pages/Create_Video';
-import DetailedVideo from './pages/Detail_Video';
+import DetailedVideo from './pages/DetailedVideo';
 import UserVideosPage from './pages/User_Videos_Page';
 import { Login, Register } from './pages/Login'; // assuming you exported login/register cleanly
 
