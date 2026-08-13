71a9f93a4c743c350d33127b335a499fea0b9fd9
elisha <elishajameel2007@gmail.com>
2026-08-13 10:06:58 +0500

    added routes in app.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import VideoUpload from './components/VideoUpload';
-import VideoList from './components/VideoList';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import CreateVideo from './pages/Create_Video';
+import DetailedVideo from './pages/Detail_Video';
+import UserVideosPage from './pages/User_Videos_Page';
+import { Login, Register } from './pages/Login'; // assuming you exported login/register cleanly
 
 export default function App() {
   return (
-    <main className="min-h-screen bg-gray-50 py-12 px-4">
-      <VideoUpload />
-      <VideoList />
-    </main>
+    <Router>
+      <div className="min-h-screen bg-white font-sans text-gray-900">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<CreateVideo />} />
+          <Route path="/watch/:id" element={<DetailedVideo />} />
+          <Route path="/profile" element={<UserVideosPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
