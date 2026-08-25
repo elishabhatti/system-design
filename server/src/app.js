e9b6dd13174dc81b99e6fe30bb8360d605f346d1
elisha <elishajameel2007@gmail.com>
2026-08-25 10:35:40 +0500

    used pu sub

--- server/src/app.js
+++ server/src/app.js
@@ -6,9 +6,14 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import http from "http";
 import { Server } from "socket.io";
+import { createAdapter } from "@socket.io/redis-adapter";
+import Redis from "ioredis";
 
 const app = express();
 const server = http.createServer(app);
+
+const pubClient = new Redis({ host: "localhost", port: 6379 });
+const subClient = pubClient.duplicate();
 
 const io = new Server(server, {
   cors: {
@@ -18,10 +23,12 @@ const io = new Server(server, {
   }
 });
 
+io.adapter(createAdapter(pubClient, subClient));
+
 app.set("io", io); 
 
 io.on("connection", (socket) => {
-  console.log("A user connected:", socket.id);
+  console.log(`A user connected on instance [Port: ${process.env.PORT || 3001}]:`, socket.id);
 
   socket.on("join_video_room", (videoId) => {
     socket.join(`video_${videoId}`);
