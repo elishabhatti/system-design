a88c4c08a82625285192ce93fdf3ec9c594ce5dc
elisha <elishajameel2007@gmail.com>
2026-09-05 10:09:38 +0500

    applied auth limiter as a middleware in the routes

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { register, login, logout, getMe, updateProfile, toggleSubscription, getAllSubscriptions } from '../controllers/user.controller.js';
 import { protect } from '../middleware/auth.middleware.js';
+import { authLimiter } from '../middleware/rateLimiter.middleware.js';
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
-router.post('/logout', logout);
+router.post('/register',authLimiter , register);
+router.post('/login', authLimiter, login);
+router.post('/logout', authLimiter, logout);
 router.get('/me', protect, getMe);
 router.put('/profile', protect, updateProfile);
 router.post('/channels/:channelId/subscribe', protect, toggleSubscription);
