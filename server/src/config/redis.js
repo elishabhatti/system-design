8dac422aea6b2913fd6080e77ee5f4461a580b6f
elisha <elishajameel2007@gmail.com>
2026-09-05 10:22:46 +0500

    added more security fo rredis as well

--- server/src/config/redis.js
+++ server/src/config/redis.js
@@ -1,11 +1,12 @@
 import Redis from 'ioredis';
 
 const redis = new Redis({
-  host: 'localhost',
-  port: 6379,
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT
 });
 
 redis.on('connect', () => {
+  console.log('✅ Redis Connected Successfully');
 });
 
 redis.on('error', (err) => {
