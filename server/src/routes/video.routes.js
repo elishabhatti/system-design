a654ac156b14f1b5abefc9e7400b627b80dd7d4b
elisha <elishajameel2007@gmail.com>
2026-09-07 09:58:25 +0500

    added api limiter in the view count

--- server/src/routes/video.routes.js
+++ server/src/routes/video.routes.js
@@ -2,13 +2,14 @@ import { Router } from 'express';
 import { getVideos, uploadVideo, deleteVideo, incrementVideoView, toggleVideoLike } from '../controllers/video.controller.js';
 import { upload } from '../config/cloudinary.js'; 
 import { protect } from '../middleware/auth.middleware.js';
+import { apiLimiter } from '../middleware/rateLimiter.middleware.js';
 
 const router = Router();
 
 router.post('/upload', protect, upload.single('video'), uploadVideo);
 router.get('/', getVideos);
 router.delete('/:id', protect, deleteVideo);
-router.post('/:id/view', protect, incrementVideoView);;
+router.post('/:id/view', apiLimiter, incrementVideoView);
 router.post('/:videoId/like', protect, toggleVideoLike);
 
 export default router;
