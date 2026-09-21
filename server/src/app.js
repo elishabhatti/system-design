65f201c873385d41449cc3611772016b7b908e8a
elisha <elishajameel2007@gmail.com>
2026-09-21 09:29:20 +0500

    used all methods of http

--- server/src/app.js
+++ server/src/app.js
@@ -25,7 +25,7 @@ const subClient = pubClient.duplicate();
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true
   }
 });
