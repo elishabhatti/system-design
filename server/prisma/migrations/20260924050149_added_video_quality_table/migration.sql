297ed0d34d6bb1f4b43fcfb494e96a73cc7ecf38
elisha <elishajameel2007@gmail.com>
2026-09-24 10:02:07 +0500

    migrated video quality table changes

---
+++ server/prisma/migrations/20260924050149_added_video_quality_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `VideoQuality` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `VideoQuality` DROP COLUMN `createdAt`;
